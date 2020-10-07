25147eac8b9968fe48d418d61ffe1d0fd29da684
fedgut <fedgut@gmail.com>
2020-10-07 16:18:18 -0500

    Refactor Binary search to handle pivoted cases

--- Pramp.com/shifted-array-search/solution.js
+++ Pramp.com/shifted-array-search/solution.js
@@ -1,18 +1,27 @@
-function binarySearch(arr, num, start = 0, end = arr.length - 1) {
+function pivotedBinarySearch(arr, num, start = 0, end = arr.length - 1) {
   // Base condition
   if (start > end) return -1;
 
   const mid = Math.floor((start + end) / 2);
-  console.log(start, end, mid);
+  const searchLeft = [arr, num, start, mid - 1];
+  const searchRight = [arr, num, mid + 1, end];
+
   // When we find num, return its index
   if (arr[mid] === num) return mid;
 
-  // if num in left, search left
-  if (num < mid) {
-    return binarySearch(arr, num, start, mid - 1);
+  // if left is sorted, see if num is in left
+  if (num < arr[mid]) {
+    if (arr[start] <= num < arr[mid]) {
+      return pivotedBinarySearch(...searchLeft);
+    } // If num is not in left, search right
+    return pivotedBinarySearch(...searchRight);
   }
-  // if num in right, search right
-  return binarySearch(arr, num, mid + 1, end);
+  // If right is sorted and num is in right, search right
+  if (arr[mid] < num <= arr[end]) {
+    return pivotedBinarySearch(...searchRight);
+  }
+  // If num is not in the right, and left is not sorted, search left
+  return pivotedBinarySearch(...searchLeft);
 }
 
-console.log(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 12));
+console.log(pivotedBinarySearch([2, 3, 4, 5, 6, 0, 1], 3));
